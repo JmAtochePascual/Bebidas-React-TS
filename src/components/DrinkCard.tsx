f80ac77bdad6d37897689e463568bcd1e3ed0bc0
JeanCarlos <jmatochepascual@gmail.com>
2025-02-07 09:48:42 -0500

    feat: :lipstick: Muestra las bebidas en pantalla
    Implementa la creación del componente DrinkCard para representar una bebida y mostrarla en pantalla

---
+++ src/components/DrinkCard.tsx
@@ -0,0 +1,29 @@
+import { TDrinkApiResponse } from "../types"
+
+type TDrinkCardProps = {
+  drink: TDrinkApiResponse;
+}
+
+const DrinkCard = ({ drink }: TDrinkCardProps) => {
+  const { strDrink, strDrinkThumb } = drink;
+  return (
+    <div className="w-full max-w-96 p-4 bg-white shadow-md rounded-md md:max-w-none">
+      <div className="w-full mb-2 overflow-hidden">
+        <img
+          src={strDrinkThumb}
+          alt={`Imagen de ${strDrink}`}
+          className="w-full transition  hover:scale-125 hover:rotate-2 transform duration-300 ease-in-out" />
+      </div>
+      <p className="max-w-60 mb-2 text-lg font-black text-gray-800 truncate">
+        {strDrink}
+      </p>
+
+      <button
+        className="w-full px-4 py-2 font-bold uppercase cursor-pointer bg-orange-500 text-white hover:bg-orange-600">
+        ver receta
+      </button>
+    </div>
+  )
+}
+
+export default DrinkCard
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,29 @@
+import DrinkCard from "../components/DrinkCard";
+import { useAppStore } from "../store/useAppStore"
 
 const HomePage = () => {
+  const { drinks } = useAppStore();
+  const isDrinksEmpty = drinks.length === 0;
+
   return (
-    <>
-      Inicio
-    </>
+    <section>
+      <h1 className="mb-10 text-4xl font-bold text-gray-800 md:text-5xl">Recetas</h1>
+
+      {
+        isDrinksEmpty
+          ? <p className="text-lg text-center text-gray-800">No hay recetas, Inicia realizando una búsqueda</p>
+          : <div className="grid gap-4 justify-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {
+              drinks.map(drink =>
+                <DrinkCard
+                  key={drink.idDrink}
+                  drink={drink}
+                />
+              )
+            }
+          </div>
+      }
+    </section>
   )
 }
 
